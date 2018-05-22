d85b0f1a1b218b63e8d01378a8184eb9e14cff24
Steffen Köhler <steffen.koehler@sovanta.com>
2018-05-22 10:27:53 +0200

    Configuring Redux Saga

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -13,7 +13,7 @@ import { ClearButton } from '../components/Buttons';
 import { LastConverted } from '../components/Text';
 import { Header } from '../components/Header';
 
-import { swapCurrency, changeCurrencyAmount } from '../actions/currencies';
+import { swapCurrency, changeCurrencyAmount, getInitialConversion } from '../actions/currencies';
 
 class Home extends Component {
     static propTypes = {
@@ -26,6 +26,10 @@ class Home extends Component {
         isFetching: PropTypes.bool,
         lastConvertedDate: PropTypes.object,
         primaryColor: PropTypes.string,
+    }
+
+    componentWillMount() {
+        this.props.dispatch(getInitialConversion());
     }
 
     handlePressBaseCurrency = () => {
