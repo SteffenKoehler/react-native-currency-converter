2c12c16eab680197f2b310c1539c5f95cf484d03
Steffen Köhler <steffen.koehler@sovanta.com>
2018-05-22 09:22:25 +0200

    Currency List Screen: Reading Currency Data from the Store

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -8,12 +8,12 @@ import currencies from '../data/currencies';
 
 import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
-const TEMP_CURRENT_CURRENCY = 'CAD';
-
 class CurrencyList extends Component {
     static propTypes = {
         navigation: PropTypes.object,
         dispatch: PropTypes.func,
+        baseCurrency: PropTypes.string,
+        quoteCurrency: PropTypes.string,
     }
 
     handlePress = (currency) => {
@@ -29,6 +29,11 @@ class CurrencyList extends Component {
     };
 
     render() {
+        let comparisonCurrency = this.props.baseCurrency;
+        if (this.props.navigation.state.params.type === 'quote') {
+            comparisonCurrency = this.props.quoteCurrency;
+        }
+
         return (
             <View style={{ flex: 1 }}>
                 <StatusBar barStyle="default" translucent={false} />
@@ -38,7 +43,7 @@ class CurrencyList extends Component {
                     renderItem={({ item }) => (
                         <ListItem
                             text={item}
-                            selected={item === TEMP_CURRENT_CURRENCY}
+                            selected={item === comparisonCurrency}
                             onPress={() => this.handlePress(item)}
                         />
                     )}
@@ -50,4 +55,17 @@ class CurrencyList extends Component {
     }
 }
 
-export default connect()(CurrencyList);
+// const mapStateToProps = (state) => {
+//     return {
+//         baseCurrency: state.currencies.baseCurrency,
+//         quoteCurrency: state.currencies.quoteCurrency,
+//     };
+// };
+
+// can be written as
+const mapStateToProps = state => ({
+    baseCurrency: state.currencies.baseCurrency,
+    quoteCurrency: state.currencies.quoteCurrency,
+});
+
+export default connect(mapStateToProps)(CurrencyList);
